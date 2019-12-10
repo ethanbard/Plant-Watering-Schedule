2b6973156c11176dd4dcd47b3dcad521ad35ac30
ethanbard <ethan.r.bard@gmail.com>
2019-12-09 21:46:00 -0600

    app can now receive JSON data

--- schedule-app/src/App.js
+++ schedule-app/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PostData from './data/Apprentice_WeGrowInTandem_Data.json';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,6 +13,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Tandem Plant Watering Schedule</h1>
+        {PostData.map((postDetail, index) =>{
+          return <h2>Water {postDetail.name} every {postDetail.water_after}</h2>
+        })}
       </div>
     );
   }
