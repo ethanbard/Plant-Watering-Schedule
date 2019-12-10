b8fe2db8f2446b5cf4ee1d5dc609d6fb6740d882
ethanbard <ethan.r.bard@gmail.com>
2019-12-09 20:56:24 -0600

    Initial commit

---
+++ schedule-app/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Tandem Plant Watering Schedule</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
